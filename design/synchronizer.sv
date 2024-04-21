fd76c15e491c311cba2b9f9bf1a1947f88d02c77
SohaibAlaraby <110909784+SohaibAlaraby@users.noreply.github.com>
2024-04-21 22:09:04 +0200

    Update synchronizer.sv

--- design/synchronizer.sv
+++ design/synchronizer.sv
@@ -1,1 +1,19 @@
+module syncronizer(
+    input bit clk,
+    input bit reset,
+    input logic data_in,
+    output logic data_out
+);
 
+reg [1:0] state, next_state;
+always@(posedge clk or posedge reset)
+begin
+    if(reset)
+        state <= 2'b11;
+    else
+        state <= next_state;
+end
+assign next_state[0] = data_in;
+assign next_state[1] = state[0];
+assign data_out = state[1];
+endmodule 
